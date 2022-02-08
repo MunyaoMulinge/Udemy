96b3253e4bdbeca098fb6940e2422d9c2ce0bf6e
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-08 11:10:09 +0300

    changes

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,10 @@
-number = eval(input("Enter Number\n"))
-sqrt = number * number
-print(sqrt)
+#  number = eval(input("Enter Number\n"))
+#  sqrt = number * number
+#  print(sqrt)
+import math
+print("Number \t\t Square")
+print("1 \t\t\t "+str(math.pow(1, 2)))
+print("2 \t\t\t "+str(math.pow(2, 2)))
+print("3 \t\t\t "+str(math.pow(3, 2)))
+print("4 \t\t\t "+str(math.pow(4, 2)))
+print("5 \t\t\t "+str(math.pow(5, 2)))
