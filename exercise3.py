8316d3a0c8fb10dd3be0b306230c857c872b1af2
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-08 13:03:37 +0300

    changes

---
+++ exercise3.py
@@ -0,0 +1,7 @@
+name = input("Enter name\n")
+age = input("Enter age\n")
+
+if name == "Faisal Zamir" and age == "25":
+    print(f"Hi {name}!, your age is {age}")
+else:
+    print("Not the one I'm looking for.")
