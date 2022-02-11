838bbd822f86597f13ba31fab0ccfafad5d6012d
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-11 17:12:52 +0300

    udemy

--- exercise14.py
+++ exercise14.py
@@ -1,13 +1,13 @@
 numb1 = int(input("Enter the first number:\n"))
-signs = input("Enter sign:\n")
 numb2 = int(input("Enter the second number:\n"))
-# signs = input("Enter sign:\n")
-
+signs = input("Enter sign:\n")
 if signs == "+":
     print(numb1+numb2)
-if signs == "-":
+elif signs == "-":
     print(numb1-numb2)
-if signs == "*":
+elif signs == "*":
     print(numb1*numb2)
-if signs == "/":
+elif signs == "/":
     print(numb1/numb2)
+else:
+    print("Invalid Operand")
