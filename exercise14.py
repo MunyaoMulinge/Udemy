3138a4b1094b382720d7c045a529128f51c412de
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-11 16:55:33 +0300

    udemy

---
+++ exercise14.py
@@ -0,0 +1,13 @@
+numb1 = int(input("Enter the first number:\n"))
+signs = input("Enter sign:\n")
+numb2 = int(input("Enter the second number:\n"))
+# signs = input("Enter sign:\n")
+
+if signs == "+":
+    print(numb1+numb2)
+if signs == "-":
+    print(numb1-numb2)
+if signs == "*":
+    print(numb1*numb2)
+if signs == "/":
+    print(numb1/numb2)
